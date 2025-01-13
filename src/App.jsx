d9e0c7f43411fe3607d8351c59e81a904b3895c7
nikiiigoyal <nikiiigoyal@gmail.com>
2025-01-13 21:49:24 +0530

    added tanstack router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-// rename the file App.jsx
-// delete the React import
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Order from "./Order";
-import { StrictMode, useState } from "react";
-import PizzaOfTheDay from "./PizzaOfTheDay";
-import Header from "./Header";
-import { CartContext } from "./contexts";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-// delete the Pizza component
+const router = createRouter({ routeTree });
 
 const App = () => {
-  const cartHook = useState([]);
   return (
     <StrictMode>
-      <CartContext.Provider value={cartHook}>
-        <div>
-          <Header />
-          <Order />
-          <PizzaOfTheDay />
-        </div>
-      </CartContext.Provider>
+      <RouterProvider router={router} />
     </StrictMode>
   );
 };

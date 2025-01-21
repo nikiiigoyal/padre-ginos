9cb6f9eab7b4f4b34bf566fe4c1e23f407404b33
nikiiigoyal <nikiiigoyal@gmail.com>
2025-01-21 23:03:01 +0530

    changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./../public/style.css";
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient({

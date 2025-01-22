8654bd42e6c881222e514b9b8d46cae31ed80da3
nikiiigoyal <nikiiigoyal@gmail.com>
2025-01-22 21:48:49 +0530

    changes in app

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,18 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/public": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:3000",
+  //       changeOrigin: true,
+  //     },
+  //     "/public": {
+  //       target: "http://localhost:3000",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   plugins: [
     TanStackRouterVite(),
     react({

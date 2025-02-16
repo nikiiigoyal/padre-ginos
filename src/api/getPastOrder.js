b5746cb1a908697bdd638e15f0cae486fe3b5f3c
nikiiigoyal <nikiiigoyal@gmail.com>
2025-02-17 01:24:14 +0530

    add APi url

--- src/api/getPastOrder.js
+++ src/api/getPastOrder.js
@@ -1,6 +1,8 @@
 export default async function getPastOrder(order) {
   await new Promise((resolve) => setTimeout(resolve, 10000));
-  const response = await fetch(`/api/past-order/${order}`);
+
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const response = await fetch(`${apiUrl}/api/past-order/${order}`);
   const data = await response.json();
   return data;
 }

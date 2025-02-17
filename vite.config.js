5f307490e88df8fd9ee3880c67c9111469cde424
nikiiigoyal <nikiiigoyal@gmail.com>
2025-02-17 11:04:31 +0530

    changes in configuration

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,13 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react({
-      babel: {
-        plugins: [
-          [
-            "babel-plugin-react-compiler",
-            {
-              target: "19",
-            },
-          ],
-        ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
       },
-    }),
-  ],
-  // No need to define environment variables here
+    },
+  },
+  plugins: [TanStackRouterVite(), react()],
 });
